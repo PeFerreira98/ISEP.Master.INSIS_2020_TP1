7ffc188ce63fc90a24242ad8b2afe6b5320d5c02
Filipe Ferreira <1160826@isep.ipp.pt>
2020-04-10 19:31:05 +0100

    ComplexType project

---
+++ ComplexType/src/main/java/SimpleTableType.java
@@ -0,0 +1,25 @@
+import org.activiti.engine.form.AbstractFormType;
+
+@SuppressWarnings("unchecked")
+public class SimpleTableType extends AbstractFormType {
+    private static final long serialVersionUID = 1L;
+
+    public static final String TYPE_NAME = "simpleTable";
+
+    public String getName() {
+        return TYPE_NAME;
+    }
+
+    @Override
+    public Object convertFormValueToModelValue(String propertyValue) {
+        return propertyValue;
+    }
+
+    @Override
+    public String convertModelValueToFormValue(Object modelValue) {
+        if	(modelValue	== null) {
+            return null;
+        }
+        return modelValue.toString();
+    }
+}
