a2b7403acabdf907820c5cd229407d4e53a7d2b8
Pedro Ferreira <1140953@isep.ipp.pt>
2020-04-13 02:09:40 +0100

    almighty commit

--- CustomFiles/rascunho.js
+++ CustomFiles/rascunho.js
@@ -1,5 +1,6 @@
 
-print("----mainScrpt3-----");
+
+print("----mainScrpt7-----");
 print("titulo: " + titulo1);
 print("anopublicacao: " + anopublicacao1);
 print("areaConhecimento: " + areasconhecimento1);
@@ -9,7 +10,49 @@ print("peritos1: " + peritos1);
 print("peritos2: " + peritos2);
 print("facts: " + selectedFacts);
 print("permited: " + isPermited);
-print("----mainScrpt3-----");
+print("permited1: " + isPermitedDispFin);
+print("permited2: " + isPermitedDecideAquisicao);
+print("permited3: " + isPermitedElaboraPAD);
+print("permited4: " + isPermitedAprovaPAD);
+print("----mainScrpt7-----");
+
+
+var p = 1;
+execution.setVariable('isPermitedAprovaPAD', p);
+
+
+
+var c1 = JSON.parse(execution.getVariable('obraInPolosDetail'));
+var s1 = '';
+for (var i = 0; i < c1.length; i++) {
+    s1 += '[';
+    s1 += c1[i].name;
+    for (var j = 0; j < c1[i].obras.length; j++) {
+        s1 += '[';
+        s1 += c1[i].obras[j].estado;
+        s1 += ':'
+        s1 += c1[i].obras[j].valor;
+        s1 += ']';
+    }
+    s1 += ']';
+}
+execution.setVariable('obrasDetail', s1);
+
+
+print("----mainScrpt5-----");
+print("titulo: " + titulo1);
+print("anopublicacao: " + anopublicacao1);
+print("areaConhecimento: " + areasconhecimento1);
+print("polo1: " + polo1);
+print("autores: " + autores1);
+print("peritos1: " + peritos1);
+print("peritos2: " + peritos2);
+print("facts: " + selectedFacts);
+print("permited: " + isPermited);
+print("permited1: " + isPermitedDispFin);
+print("permited2: " + isPermitedDecideAquisicao);
+print("----mainScrpt5-----");
+
 
 var totalPeritos = (execution.getVariable('registeredUsers')).length;
 var selectedFacts = execution.getVariable('selectedFacts');
@@ -24,8 +67,8 @@ for (var index = 0; index < selectedFacts.length; index++) {
     }
 }
 
-var isPermited= 0;
-if ((totalPeritos/2) < fact1 ) {
+var isPermited = 0;
+if ((totalPeritos / 2) < fact1) {
     isPermited = 1;
 }
 
@@ -42,12 +85,12 @@ var peritosToDelete = [];
 for (var index = 0; index < registeredUsers.length; index++) {
     var bool = false;
     for (var i = 0; i < peritosSelectedByProponente.length; i++) {
-        if (JSON.stringify(registeredUsers[index]) == JSON.stringify(peritosSelectedByProponente[i]) ) {
+        if (JSON.stringify(registeredUsers[index]) == JSON.stringify(peritosSelectedByProponente[i])) {
             bool = true;
         }
     }
     for (var i = 0; i < peritosSelectedByBiblioMor.length; i++) {
-        if (JSON.stringify(registeredUsers[index]) == JSON.stringify(peritosSelectedByBiblioMor[i]) ) {
+        if (JSON.stringify(registeredUsers[index]) == JSON.stringify(peritosSelectedByBiblioMor[i])) {
             bool = true;
         }
     }
@@ -66,7 +109,7 @@ print(registeredUsers);
 
 var chooseList = JSON.parse("[{\"_id\": 0, \"text\": \"Negativo\"},{\"_id\": 1, \"text\": \"Positivo\"}]");
 print(JSON.stringify(chooseList));
-execution.setVariable('chooseList',JSON.stringify(chooseList));
+execution.setVariable('chooseList', JSON.stringify(chooseList));
 
 
 
@@ -77,11 +120,11 @@ var peritosParsed = [];
 for (var index = 0; index < peritosAll.length; index++) {
     var bool = false;
     for (var index2 = 0; index2 < peritosSelected.length; index2++) {
-        if(peritosAll[index].text == peritosSelected[index2].text){
+        if (peritosAll[index].text == peritosSelected[index2].text) {
             bool = true;
         }
     }
-    if(bool == false){
+    if (bool == false) {
         peritosParsed.push(peritosAll[index]);
     }
 }
